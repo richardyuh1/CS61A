5280e9d645444346ee2a4b3f8874d4f9a66bae86
richardyuh1 <40287849+richardyuh1@users.noreply.github.com>
2019-05-01 11:10:33 -0700

    Add files via upload

---
+++ hw04/problems/hw04.py
@@ -0,0 +1,174 @@
+HW_SOURCE_FILE = 'hw04.py'
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    if n<=3:
+        return n
+    else:
+        return g(n - 1) + 2 * g(n - 2) + 3 * g(n - 3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n<=3:
+        return n
+    a = 1
+    b = 2
+    c = 3
+    
+    while n > 3:
+        a,b,c = b,c,3*a + 2*b + c
+        n -= 1
+
+    return c
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def pingpongnumber(k, num, direction = 'up'):
+        if k == n:
+            return num
+        if direction == 'up':
+            return switch(k+1, num+1, direction)
+        if direction == 'down':
+            return switch(k+1,num-1,direction)
+
+    def switch(k, num, direction):
+        if has_seven(k) or k%7==0:
+            if direction == 'up':
+                return pingpongnumber(k,num,'down')
+            if direction == 'down':
+                return pingpongnumber(k,num,'up')
+        else:
+            return pingpongnumber(k,num,direction)
+
+    return pingpongnumber(1,1,'up')
+
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    "*** YOUR CODE HERE ***"
+    def count_partitions(amount, m):
+        if amount == 0:
+            return 1
+        elif amount < 0:
+            return 0
+        elif m > amount:
+            return 0
+        else:
+            return count_partitions(amount-m, m) + count_partitions(amount, m*2)
+
+    return count_partitions(amount,1)
+
+
+###################
+# Extra Questions #
+###################
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
